a684fa02a1c2dd92ef6bca750df2db0e6e35c111
Tainá Coutinho <taicout6@gmail.com>
2025-04-27 20:34:51 -0300

    [GET clients]

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const { clientsModel } = require('./models');
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', async (_req, res) => {
+  const clients = await clientsModel.findAll();
+  return res.status(200).json(clients);
+});
 
 module.exports = app;
