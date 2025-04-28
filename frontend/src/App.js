0dec7660b3205d6de87f4ebad6c94ba8f438bd77
Tainá Coutinho <taicout6@gmail.com>
2025-04-27 22:15:29 -0300

    [Frontend][Estruturando front-end, add componentes básicos]

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Form from "./components/Form";
+import Grid from "./components/Grid";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Title>Clientes</Title>
+        <Form />
+        <Grid />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.position}/>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
